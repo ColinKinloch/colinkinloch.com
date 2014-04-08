d0ac99821b77deadcbe1c9ff14350c56ec009ed4
Colin Kinloch <colinmkinloch@gmail.com>
2014-04-08 10:55:48 +0100

    done

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('\'Allo \'Allo!');
+var cvDoc = jQuery('#cv-doc');
+jQuery.get('/cv.md', function(data){
+	cvDoc.html(data).addClass('md');
+	marked(data,{
+		tables: true
+	}, function(error, html){
+		cvDoc.html(html).removeClass('md');
+	});
+});
