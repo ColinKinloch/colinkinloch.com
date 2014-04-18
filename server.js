2e37d822ad8c2a415fda20eb7516596313f60fc9
Colin Kinloch <colinmkinloch@gmail.com>
2014-04-18 11:39:23 +0100

    not quite sure of the problem

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ var app = express();
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+app.use(express.logger());
 app.use(express.static(__dirname + '/dist'));
 app.listen(port, ipaddr);
