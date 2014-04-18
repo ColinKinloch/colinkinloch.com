2f25d02441837afec87862b8f6ac668afa542e86
Colin Kinloch <colinmkinloch@gmail.com>
2014-04-18 11:28:50 +0100

    fix

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(port);
+app.listen(port, ipaddr);
