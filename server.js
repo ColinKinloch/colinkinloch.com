6108fda3e32c52a1fa9a2fd36b1778cab50747d2
Colin Kinloch <colinmkinloch@gmail.com>
2014-04-18 11:17:08 +0100

    updated port

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ var express = require('express');
 
 var app = express();
 
+var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
 app.use(express.static(__dirname + '/dist'));
-app.listen(OPENSHIFT_NODEJS_PORT);
+app.listen(port);
